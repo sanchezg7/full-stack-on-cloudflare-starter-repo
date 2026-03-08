d5f35cdcdcbea8f66e5f0e0bd981ad5a4df0172b
sanchezg7 <gsanc.tech@pm.me>
2026-03-08 17:56:17 -0400

    wip: adding better auth

--- packages/data-ops/auth-gen/auth.ts
+++ packages/data-ops/auth-gen/auth.ts
@@ -1,6 +1,13 @@
 import { betterAuth } from "better-auth";
 import Database from "better-sqlite3";
 
+/**
+ * Better auth can generate schemas for drizzle on our behalf
+ */
+
+/**
+ * https://better-auth.com/docs/basic-usage
+ */
 export const auth = betterAuth({
   database: new Database("./sqlite.db"),
 });
