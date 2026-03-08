31dbb897464cdb828d23ac358c1df53c74e9f498
sanchezg7 <gsanc.tech@pm.me>
2026-03-08 13:41:36 -0400

    wip: wire up the app module

--- apps/user-application/worker/index.ts
+++ apps/user-application/worker/index.ts
@@ -1,24 +1,25 @@
-import {fetchRequestHandler} from "@trpc/server/adapters/fetch";
-import {appRouter} from "./trpc/router";
-import {createContext} from "./trpc/context";
+import {App} from "./hono/app";
 import {initDatabase} from "@repo/data-ops/database";//"@repo/data-ops/database";
 
 export default {
     fetch(request, env, ctx) {
         initDatabase(env.DB)
-        const url = new URL(request.url);
-
-        if (url.pathname.startsWith("/trpc")) {
-            return fetchRequestHandler({
-                endpoint: "/trpc",
-                req: request,
-                router: appRouter,
-                createContext: () =>
-                    createContext({req: request, env: env, workerCtx: ctx}),
-            });
-        }
-        // We want to proxy /click-socket to the data-service
-
-        return env.ASSETS.fetch(request);
+        return App.fetch(request, env, ctx);
+        // This is commented out because we've moved it to the App module
+        // const url = new URL(request.url);
+        //
+        //
+        // if (url.pathname.startsWith("/trpc")) {
+        //     return fetchRequestHandler({
+        //         endpoint: "/trpc",
+        //         req: request,
+        //         router: appRouter,
+        //         createContext: () =>
+        //             createContext({req: request, env: env, workerCtx: ctx}),
+        //     });
+        // }
+        // // We want to proxy /click-socket to the data-service
+        //
+        // return env.ASSETS.fetch(request);
     },
 } satisfies ExportedHandler<ServiceBindings>;
