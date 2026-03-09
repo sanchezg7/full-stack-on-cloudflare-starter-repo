4718d8ae18c0b508031b953202e5d798146b699b
sanchezg7 <gsanc.tech@pm.me>
2026-03-08 20:55:10 -0400

    wip: ready for staging

--- apps/user-application/worker/hono/app.ts
+++ apps/user-application/worker/hono/app.ts
@@ -5,42 +5,55 @@ import { appRouter } from "@/worker/trpc/router.ts";
 import { createContext } from "@/worker/trpc/context";
 import { getAuth } from "@repo/data-ops/auth";
 
-export const App = new Hono<{ Bindings: ServiceBindings }>();
+export const App = new Hono<{ Bindings: ServiceBindings, Variables: { userId: string }}>();
+
+const getAuthInstance = (env: Env) => {
+    // this is where you can add provider details, as needed, when you add new ones.
+    return getAuth({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET
+    });
+}
 
 /**
  * We only want to protect trpc, we want to auth to not be blocked (so that people can actually auth)
  */
 const authMiddleware = createMiddleware(async (c, next) => {
-    console.log("Auth Middleware Called");
+    const auth = getAuthInstance(c.env);
+    const session = await auth.api.getSession({ headers: c.req.raw.headers });
+    if(!session?.user) {
+        return c.text("Unauthorized", 401);
+    }
+    const userId = session.user.id;
+    c.set("userId", userId);
     await next();
 })
 
 App.all("/trpc/*", authMiddleware, (c) => {
+    const userId = c.get("userId");
     return fetchRequestHandler({
         endpoint: "/trpc",
         req: c.req.raw,
         router: appRouter,
         createContext: () => {
-            return createContext({ req: c.req.raw, env: c.env, workerCtx: c.executionCtx })
+            return createContext({ req: c.req.raw, env: c.env, workerCtx: c.executionCtx, userId })
         }
     });
 });
 
 App.get("/click-socket", authMiddleware, async (c) => {
+    // you could isolate per account (where one account can be many users)
+    const userId = c.get("userId");
     const headers = new Headers(c.req.raw.headers);
     // We will manually set this until the authentication logic is implemented
-    headers.set("account-id", "1234567890");
+    headers.set("account-id", userId);
     const proxiedRequest = new Request(c.req.raw.url, { headers });
     return c.env.BACKEND_SERVICE.fetch(proxiedRequest);
 })
 
 // https://better-auth.com/docs/integrations/hono
 App.on(["POST", "GET"], "/api/auth/*", (c) => {
-    // this is where you can add provider details, as needed, when you add new ones.
-    const auth = getAuth({
-        clientId: c.env.GOOGLE_CLIENT_ID,
-        clientSecret: c.env.GOOGLE_CLIENT_SECRET
-    });
+    const auth = getAuthInstance(c.env)
 
     return auth.handler(c.req.raw);
 })
