a8087aa5f55111d64c366c28ce66b37d9360c85e
sanchezg7 <gsanc.tech@pm.me>
2026-03-12 21:36:04 -0400

    wip: added stripe plugin. need to create stripe account now

--- packages/data-ops/src/auth.ts
+++ packages/data-ops/src/auth.ts
@@ -2,8 +2,25 @@ import { betterAuth } from "better-auth";
 import {drizzleAdapter} from "better-auth/adapters/drizzle";
 import {getDb} from "@/db/database";
 import {account, session, user, verification} from "@/drizzle-out/auth-schema";
+// use stripe plugin
+import { stripe } from "@better-auth/stripe";
+// use stripe SDK
+import Stripe from "stripe";
 
 let auth: ReturnType<typeof betterAuth>;
+
+/**
+ * We make this type in order to make it extensible
+ */
+type StripeConfig = {
+    stripeWebhookSecret: string;
+    plans: any[];
+}
+
+const stripeClient = new Stripe("", {
+    // this was auto completed
+    apiVersion: "2025-03-31.basil",
+})
 
 /**
  * Better auth can generate schemas for drizzle on our behalf
@@ -11,6 +28,7 @@ let auth: ReturnType<typeof betterAuth>;
  */
 export function createBetterAuth(
     database: NonNullable<Parameters<typeof betterAuth>[0]>["database"],
+    stripeConfig?: StripeConfig,
     google?: { clientId: string; clientSecret: string },
 ): ReturnType<typeof betterAuth> {
     return betterAuth({
@@ -23,11 +41,27 @@ export function createBetterAuth(
                 clientId: google?.clientId ?? "",
                 clientSecret: google?.clientSecret ?? "",
             }
-        }
+        },
+        plugins: [
+            stripe({
+                stripeClient,
+                stripeWebhookSecret: stripeConfig?.stripeWebhookSecret ?? process.env.STRIPE_WEBHOOK_SECRET!,
+                // put the customer in stripe when the user signs up for the first time in our app. This will power the new schema for the users table
+                createCustomerOnSignUp: true,
+                subscription: {
+                    enabled: true,
+                    plans: stripeConfig?.plans ?? []
+                }
+            })
+        ]
     });
 };
 
-export function getAuth(google: { clientId: string; clientSecret: string }): ReturnType<typeof betterAuth>  {
+export function getAuth(
+    google: { clientId: string; clientSecret: string },
+    stripe: StripeConfig,
+
+): ReturnType<typeof betterAuth>  {
     if (auth) {
         return auth;
     }
@@ -43,6 +77,7 @@ export function getAuth(google: { clientId: string; clientSecret: string }): Ret
                 verification,
             }
         }),
+        stripe,
         google
     );
 
